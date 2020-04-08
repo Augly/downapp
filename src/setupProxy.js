a5b365e8bb79de6c40d4232f8983f63508a2303e
仇益阳 <1137774080@qq.com>
2020-04-08 17:38:24 +0800

    提交代码

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,16 +3,22 @@
  * @version: 
  * @Author: zero
  * @Date: 2020-03-26 10:42:23
- * @LastEditors: zero
- * @LastEditTime: 2020-03-26 11:29:19
+ * @LastEditors: your name
+ * @LastEditTime: 2020-04-08 16:12:22
  */
 const proxy = require('http-proxy-middleware')
 
+let env = 'dev'
+const Object = {
+  pro: 'http://www.fenghuoxia.com/api/',
+  test: 'http://test.fenghuoxia.com/api/',
+  dev: 'http://dev.fenghuoxia.com/api/'
+}
 module.exports = function (app) {
   // proxy第一个参数为要代理的路由
   // 第二参数中target为代理后的请求网址，changeOrigin是否改变请求头，其他参数请看官网
   app.use(proxy('/api', {
-    target: 'http://39.107.78.120:8083/okcloud/',
+    target: Object[env],
     ws: false,
     secure: false,
     changeOrigin: true,
